4bf18be89c3af52fc0e6f4d50dd2d1ee86c08c5e
malinkang <malinkang@ushareit.com>
2021-12-14 16:48:18 +0800

    update

--- 78/test.py
+++ 78/test.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+
+
+
 for x in os.listdir('.'):
     print("* "+x)
     if(os.path.isdir(x)):
         for y in os.listdir(x):
-            print(" * ["+y.replace("-.md","")+"](78/"+x+"/"+y+")")
+            print(" * ["+y.replace(".md","")+"](78/"+x+"/"+y+")")
